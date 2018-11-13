bfffd3a8bae8a437ef5e1a0d271d783e2bc28d24
robwuk <44876521+robwuk@users.noreply.github.com>
2018-11-13 14:33:04 +0000

    Updated After Vaersion Mixup

--- MPC_Script.js
+++ MPC_Script.js
@@ -1,21 +1,19 @@
-<<<<<<< HEAD
+
 var elem = document.getElementById("name");
-=======
 /*function alertAge(age, thresh) {
     var isAdult = (age < thresh) ? "too young": "old enough";
 >>>>>>> 6c60562f8dc700969c5fcd8b3acda67ad1fb402c
-
+*/
 function person(name, age, color) {
   this.name = name;
-  this.age = age;
-  this.favColor = color;
-  this.year  = bornYear;
+//  this.age = age;
+//  this.favColor = color;
+//  this.year  = bornYear;
 }
-<<<<<<< HEAD
 
-function bornYear() {
-  return 2018 - this.age;
-}
+//function bornYear() {
+//  return 2018 - this.age;
+//}
 
 function printDetail() {
   elem.innerHTML = /*"Name: " +*/ p1.name // + "\nAge: " + p1.age + " (" + p1.year() + ")\nFavourite Colour: " + p1.favColor;
@@ -24,6 +22,3 @@ function printDetail() {
 var p1 = new person(prompt("Please Enter Your Name: "), 46, "Blue");
 
 printDetail();
-=======
-*/
->>>>>>> 6c60562f8dc700969c5fcd8b3acda67ad1fb402c
