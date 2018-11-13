6c60562f8dc700969c5fcd8b3acda67ad1fb402c
robwuk <44876521+robwuk@users.noreply.github.com>
2018-11-13 14:17:32 +0000

    Update MPC_Script.js

--- MPC_Script.js
+++ MPC_Script.js
@@ -1,6 +1,6 @@
-function alertAge(age, thresh) {
+/*function alertAge(age, thresh) {
     var isAdult = (age < thresh) ? "too young": "old enough";
 
     alert("you are " + isAdult + " (" + age + "). Theshold is " + thresh +".");
 }
-
+*/
